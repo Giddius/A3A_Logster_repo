f46d229e01bf904572b72ea706aba585c1d1d717
Giddius <GordonGrossmann@gmx.at>
2020-07-20 17:31:12 +0200

    first upload

---
+++ A3A_Logster/a3a_logster_main.py
@@ -0,0 +1,83 @@
+import os
+import jinja2
+from jinja2 import Environment
+# needs to be installed!
+import stdiomask
+
+
+_cwd = os.getcwd()
+
+
+def get_username_password():
+    """
+    gets username and password from user input,
+    password is obscured by '*' via the stdiomask module
+    Returns a tuple where [0] = user_name and [1] is password
+    """
+    user_name = input("Please enter your Username: ")
+    password = stdiomask.getpass("Please enter your Password: ")
+    return (user_name, password)
+
+
+def get_template(in_template='logster_script_template.py.jinja'):
+    """
+    gets the template as string from a file,
+    filename can be specified but also defaults if not specified
+    Returns the template object
+    """
+    with open(os.path.join(_cwd, in_template), 'r') as jinfile:
+        _template_string = jinfile.read()
+    return Environment(loader=jinja2.BaseLoader).from_string(_template_string)
+
+
+def get_vars(in_file):
+    """
+    reads a csv in and splits it into a list of dictionaries.
+    name of the csv is the 'in_file' argument and the file has to be in the same folder as the script
+    """
+    var_list = []
+    with open(os.path.join(_cwd, in_file), 'r') as csvfile:
+        _csv_list = csvfile.read().splitlines(False)
+
+    for index, line in enumerate(_csv_list):
+        if index != 0:
+            _var_dict = {}
+            Server, Category, TARGET_SERVER, Folder, Full_log, Filtered_Log = line.split(';')
+            _var_dict['target_server'] = TARGET_SERVER.strip()
+            _var_dict['ftp_var'] = Folder.strip()
+            _var_dict['server'] = Server.strip().replace(' ', '_')
+            _var_dict['category'] = Category.strip().replace(' ', '')
+            _var_dict['folder'] = Full_log.replace('\\', ' /').strip().replace(' ', '_')
+            _var_dict['filtered_folder'] = Filtered_Log.replace('\\', '/').strip().replace(' ', '_')
+
+            var_list.append(_var_dict)
+    return var_list
+
+
+def render_scripts(in_username, in_password, in_variable_dict, in_template, in_target_folder='scripts'):
+    """
+    creates the scripts with jinja, creates the 'in_target_folder(default='scripts')' if it does not already exists.
+    the scripts name is built from the variables.
+    """
+    data = in_template.render(user_name=in_username, password=in_password, **in_variable_dict)
+    folder = os.path.join(_cwd, in_target_folder)
+    script_name = f"{in_variable_dict['server']}_{in_variable_dict['category']}_script.py"
+    if os.path.exists(folder) is False:
+        os.mkdir(folder)
+    with open(os.path.join(folder, script_name), 'w') as scriptfile:
+        scriptfile.write(data)
+
+
+def main():
+    # get username and password
+    username, password = get_username_password()
+    # get the template from the template file
+    template = get_template()
+    # loop through the list of variable dictionaries from the csv
+    # and create the scripts
+    for var_group in get_vars('Mappe1.csv'):
+        render_scripts(username, password, var_group, template)
+
+
+# execute the main function
+main()
---
+++ A3A_Logster/scripts/Main_2_Server_script.py
@@ -0,0 +1,79 @@
+from ftplib import FTP
+import pathlib
+import os
+
+
+USERNAME = "test_user"
+PASSWORD = "test_pass"
+TARGET_SERVER = "antistasi.armahosts.com_2322"
+LOCAL_LOG_DIR = pathlib.Path("/www/htdocs/w0170fba/antistasi.de/dev_drive/Bob_Murphy/Full_Logs")
+LOCAL_FILTERED_LOGS_DIR = pathlib.Path("/www/htdocs/w0170fba/antistasi.de/dev_drive/Bob_Murphy/Antistasi_Community_Logs")
+
+
+def filter_file(inputPathObj, outputPathObj):
+    with inputPathObj.open("r", encoding='utf-8', errors='ignore') as input_file:
+        with outputPathObj.open("w", encoding='utf-8', errors='ignore') as output_file:
+            new_f = input_file.readlines()
+            for line in new_f:
+                if "Server: Object" not in line:
+                    if "Server: Network message" not in line:
+                        if "Client: Object" not in line:
+                            if "Error: Object" not in line:
+                                if "Setting invalid pitch" not in line:
+                                    if "Client: Remote object" not in line:
+                                        if "Error in expression <];testArray = testArray + [1];testArray set [_a, 1];testArray pushBack 1;}, 0, 1>" not in line:
+                                            if "Error position: <set [_a, 1];testArray pushBack 1;}, 0, 1>" not in line:
+                                                if "Error Type Any, expected Number" not in line:
+                                                    if "Ref to nonnetwork object" not in line:
+                                                        if "Message not sent - error 0, message ID" not in line:
+                                                            if "NetServer: cannot find channel #" not in line:
+                                                                if "NetServer: trying to send a too large non-guaranteed message" not in line:
+                                                                    output_file.write(line)
+            output_file.truncate()
+
+^
+download_single_server = TARGET_SERVER != ""
+
+ftp = FTP()
+ftp.connect(host="antistasi.armahosts.com", port=8821)
+
+ftp.login(user=USERNAME, passwd=PASSWORD)
+
+servers = [TARGET_SERVER] if download_single_server else ftp.nlst()
+
+print(servers)
+
+for server in servers:
+    print("Downloading files from server {}".format(server))
+
+    rptDir = LOCAL_LOG_DIR.joinpath('')
+    rptDir.mkdir(parents=True, exist_ok=True)
+    print(str(rptDir) + ' save location for full logs')
+
+
+    filteredRptDir = LOCAL_FILTERED_LOGS_DIR.joinpath('')
+    filteredRptDir.mkdir(parents=True, exist_ok=True)
+    print(str(filteredRptDir) + ' save location for filtered_logs')
+
+    ftp.cwd("/{}/ARMAHOSTS".format(server))
+
+    localRpts = {x.name: x.stat().st_size for x in rptDir.iterdir() if x.is_file()}
+    remoteRpts = {file_name: ftp.size(file_name) for file_name in ftp.nlst() if os.path.splitext(file_name)[1] == ".rpt"}
+
+    print("Remote Rpts: {}".format(remoteRpts))
+
+    rptsNeeded = [file_name for file_name in remoteRpts.keys() if not (file_name in localRpts) or (localRpts[file_name] != remoteRpts[file_name])]
+    print("Rpts needed: {}".format(rptsNeeded))
+
+    for file_name in rptsNeeded:
+        print("Downloading {}".format(file_name))
+        rptPath = rptDir.joinpath(file_name)
+        with rptPath.open(mode="wb") as fp:
+            ftp.retrbinary("RETR {}".format(file_name), fp.write)
+
+        print("Filtering {}".format(file_name))
+        filteredRptPath = filteredRptDir.joinpath(file_name)
+        filter_file(rptPath, filteredRptPath)
+
+
+print("logs downloaded")
